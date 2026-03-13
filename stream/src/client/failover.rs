93b859ed7981f5c8b70005534731ffe0f88ddb85
plan <frostyplanet@gmail.com>
2026-03-13 22:57:59 +0800

    stream: Simplify FailoverPool to break cycle ref
    Allow impl Clone for FailoverPool now.

--- stream/src/client/failover.rs
+++ stream/src/client/failover.rs
@@ -7,43 +7,51 @@ use crate::{
     Codec,
     error::{EncodedErr, RpcIntErr},
 };
-use arc_swap::ArcSwapOption;
+use arc_swap::ArcSwap;
 use captains_log::filter::LogFilter;
-use crossfire::*;
+use crossfire::{AsyncRx, MTx, SendError, mpsc};
 use std::fmt;
 use std::sync::{
     Arc, Weak,
     atomic::{AtomicU64, AtomicUsize, Ordering},
 };
 
-/// A pool supports failover to multiple address with optional round_robin strategy
+/// A pool supports failover to multiple address with bias or round_robin strategy
 ///
 /// Supports async and blocking context.
 ///
 /// Only retry RpcIntErr that less than RpcIntErr::Method,
 /// currently ignore custom error due to complexity of generic.
-///
-/// NOTE: there's cycle reference inside FailoverPoolInner and it's ClientPool,
-/// don't clone FailoverPool as it has custom drop. FailoverPool should be put in Arc for usage.
-pub struct FailoverPool<F, P>(Arc<FailoverPoolInner<F, P>>)
+/// (If you need to custom failover logic, copy the code and impl your own pool.)
+pub struct FailoverPool<F, P>
 where
     F: ClientFacts,
-    P: ClientTransport;
+    P: ClientTransport,
+{
+    inner: Arc<FailoverPoolInner<F, P>>,
+}
+
+struct FailoverFacts<F>
+where
+    F: ClientFacts,
+{
+    retry_tx: MTx<mpsc::List<FailoverTask<F::Task>>>,
+    facts: Arc<F>,
+    logger: Arc<LogFilter>,
+    retry_limit: usize,
+}
 
 struct FailoverPoolInner<F, P>
 where
     F: ClientFacts,
     P: ClientTransport,
 {
-    pools: ArcSwapOption<ClusterConfig<F, P>>,
+    pools: ArcSwap<ClusterConfig<F, P>>,
     round_robin: bool,
-    facts: Arc<F>,
-    retry_limit: usize,
-    retry_tx: MTx<mpsc::List<FailoverTask<F::Task>>>,
     ver: AtomicU64,
     rr_counter: AtomicUsize,
     pool_channel_size: usize,
-    logger: Arc<LogFilter>,
+    facts: Arc<FailoverFacts<F>>,
 }
 
 struct ClusterConfig<F, P>
@@ -51,7 +59,7 @@ where
     F: ClientFacts,
     P: ClientTransport,
 {
-    pools: Vec<ClientPool<FailoverPoolInner<F, P>, P>>,
+    pools: Vec<ClientPool<FailoverFacts<F>, P>>,
     ver: u64,
 }
 
@@ -60,63 +68,60 @@ where
     F: ClientFacts,
     P: ClientTransport,
 {
+    /// Initiate the pool with multiple address.
+    /// When round_robin == true, all address in the pool will be select with equal chanced;
+    /// When round_robin == false, the first address will always be pick unless error happens.
     pub fn new(
         facts: Arc<F>, addrs: Vec<String>, round_robin: bool, retry_limit: usize,
         pool_channel_size: usize,
     ) -> Self {
         let (retry_tx, retry_rx) = mpsc::unbounded_async();
+        let retry_logger = facts.new_logger();
+        let wrapped_facts =
+            Arc::new(FailoverFacts { retry_limit, retry_tx, logger: facts.new_logger(), facts });
+        let mut pools = Vec::with_capacity(addrs.len());
+        for addr in addrs.iter() {
+            let pool = ClientPool::new(wrapped_facts.clone(), addr, pool_channel_size);
+            pools.push(pool);
+        }
         // NOTE: the ClientPool has cycle reference with FailoverPoolInner
         let inner = Arc::new(FailoverPoolInner::<F, P> {
-            pools: ArcSwapOption::new(None),
+            pools: ArcSwap::new(Arc::new(ClusterConfig { ver: 0, pools })),
             round_robin,
-            facts: facts.clone(),
-            retry_limit,
-            retry_tx,
+            facts: wrapped_facts,
             ver: AtomicU64::new(1),
             rr_counter: AtomicUsize::new(0),
             pool_channel_size,
-            logger: facts.new_logger(),
         });
-        let mut pools = Vec::with_capacity(addrs.len());
-        for addr in addrs.iter() {
-            let pool = ClientPool::new(inner.clone(), addr, pool_channel_size);
-            pools.push(pool);
-        }
-        inner.pools.store(Some(Arc::new(ClusterConfig { ver: 0, pools })));
-
-        let retry_logger = facts.new_logger();
         let weak_self = Arc::downgrade(&inner);
-        facts.spawn_detach(async move {
+        inner.facts.spawn_detach(async move {
             FailoverPoolInner::retry_worker(weak_self, retry_logger, retry_rx).await;
         });
-        Self(inner)
+        Self { inner }
     }
 
     pub fn update_addrs(&self, addrs: Vec<String>) {
-        let inner = &self.0;
-        let old_cluster_arc = inner.pools.load();
-        let old_pools = old_cluster_arc.as_ref().map(|c| c.pools.clone()).unwrap_or_default();
+        let inner = &self.inner;
+        let old_pools = inner.pools.load_full();
+        let mut new_pools: Vec<ClientPool<FailoverFacts<F>, P>> = Vec::with_capacity(addrs.len());
 
-        let mut new_pools = Vec::with_capacity(addrs.len());
-
-        let mut old_pools_map = std::collections::HashMap::with_capacity(old_pools.len());
-        for pool in old_pools {
+        let mut old_pools_map = std::collections::HashMap::with_capacity(old_pools.pools.len());
+        for pool in &old_pools.pools {
             old_pools_map.insert(pool.get_addr().to_string(), pool);
         }
 
         for addr in addrs {
             if let Some(reused_pool) = old_pools_map.remove(&addr) {
-                new_pools.push(reused_pool);
+                new_pools.push(reused_pool.clone());
             } else {
                 // Create a new pool for the new address
-                let new_pool = ClientPool::new(inner.clone(), &addr, inner.pool_channel_size);
+                let new_pool = ClientPool::new(inner.facts.clone(), &addr, inner.pool_channel_size);
                 new_pools.push(new_pool);
             }
         }
-
         let new_ver = inner.ver.fetch_add(1, Ordering::Relaxed) + 1;
         let new_cluster = ClusterConfig { pools: new_pools, ver: new_ver };
-        inner.pools.store(Some(Arc::new(new_cluster)));
+        inner.pools.store(Arc::new(new_cluster));
     }
 }
 
@@ -128,12 +133,13 @@ where
     #[inline]
     fn select(
         &self, round_robin: bool, rr_counter: &AtomicUsize, last_index: Option<usize>,
-    ) -> Option<(usize, &ClientPool<FailoverPoolInner<F, P>, P>)> {
+    ) -> Option<(usize, &ClientPool<FailoverFacts<F>, P>)> {
         let l = self.pools.len();
         if l == 0 {
             return None;
         }
         let seed = if let Some(index) = last_index {
+            // last_index is address with error occurs
             index + 1
         } else if round_robin {
             rr_counter.fetch_add(1, Ordering::Relaxed)
@@ -162,30 +168,28 @@ where
         while let Ok(mut task) = retry_rx.recv().await {
             if let Some(inner) = weak_self.upgrade() {
                 let cluster = inner.pools.load();
-                if let Some(cluster) = cluster.as_ref() {
-                    // if cluster config changed, restart selection
-                    let last_index = if cluster.ver == task.cluster_ver {
-                        Some(task.last_index)
-                    } else {
-                        task.cluster_ver = cluster.ver;
-                        None // restart selection
-                    };
-                    if let Some((index, pool)) =
-                        cluster.select(inner.round_robin, &inner.rr_counter, last_index)
-                    {
-                        if let Some(last) = last_index {
-                            logger_trace!(
-                                logger,
-                                "FailoverPool: task {:?} retry {}->{}",
-                                task.inner,
-                                last,
-                                index
-                            );
-                        }
-                        task.last_index = index;
-                        pool.send_req(task).await; // retry is async
-                        continue;
+                // if cluster config changed, restart selection
+                let last_index = if cluster.ver == task.cluster_ver {
+                    Some(task.last_index)
+                } else {
+                    task.cluster_ver = cluster.ver;
+                    None // restart selection
+                };
+                if let Some((index, pool)) =
+                    cluster.select(inner.round_robin, &inner.rr_counter, last_index)
+                {
+                    if let Some(last) = last_index {
+                        logger_trace!(
+                            logger,
+                            "FailoverPool: task {:?} retry {}->{}",
+                            task.inner,
+                            last,
+                            index
+                        );
                     }
+                    task.last_index = index;
+                    pool.send_req(task).await; // retry is async
+                    continue;
                 }
                 // if we are here, something is wrong, no pool available or selection failed
                 logger_debug!(logger, "FailoverPool: no next hoop for {:?}", task.inner);
@@ -199,43 +203,33 @@ where
     }
 }
 
-impl<F, P> Drop for FailoverPool<F, P>
-where
-    F: ClientFacts,
-    P: ClientTransport,
-{
-    fn drop(&mut self) {
-        // Remove cycle reference before drop
-        self.0.pools.store(None);
-    }
-}
-
 impl<F, P> Drop for FailoverPoolInner<F, P>
 where
     F: ClientFacts,
     P: ClientTransport,
 {
+    #[inline]
     fn drop(&mut self) {
-        logger_trace!(self.logger, "FailoverPool dropped");
+        logger_trace!(self.facts.logger, "FailoverPool dropped");
     }
 }
 
-impl<F, P> std::ops::Deref for FailoverPoolInner<F, P>
+/// `orb::AsyncRuntime` will follow deref to blanket impl it for wrapper types
+impl<F> std::ops::Deref for FailoverFacts<F>
 where
     F: ClientFacts,
-    P: ClientTransport,
 {
     type Target = F;
 
+    #[inline]
     fn deref(&self) -> &Self::Target {
-        &self.facts
+        self.facts.as_ref()
     }
 }
 
-impl<F, P> ClientFacts for FailoverPoolInner<F, P>
+impl<F> ClientFacts for FailoverFacts<F>
 where
     F: ClientFacts,
-    P: ClientTransport,
 {
     type Codec = F::Codec;
 
@@ -263,6 +257,17 @@ where
     }
 }
 
+impl<F, P> Clone for FailoverPool<F, P>
+where
+    F: ClientFacts,
+    P: ClientTransport,
+{
+    #[inline]
+    fn clone(&self) -> Self {
+        Self { inner: self.inner.clone() }
+    }
+}
+
 impl<F, P> ClientCaller for FailoverPool<F, P>
 where
     F: ClientFacts,
@@ -271,10 +276,9 @@ where
     type Facts = F;
 
     async fn send_req(&self, mut task: F::Task) {
-        let cluster = self.0.pools.load();
-        if let Some(cluster) = cluster.as_ref()
-            && let Some((index, pool)) =
-                cluster.select(self.0.round_robin, &self.0.rr_counter, None)
+        let cluster = self.inner.pools.load();
+        if let Some((index, pool)) =
+            cluster.select(self.inner.round_robin, &self.inner.rr_counter, None)
         {
             let failover_task = FailoverTask {
                 last_index: index,
@@ -300,10 +304,9 @@ where
 {
     type Facts = F;
     fn send_req_blocking(&self, mut task: F::Task) {
-        let cluster = self.0.pools.load();
-        if let Some(cluster) = cluster.as_ref()
-            && let Some((index, pool)) =
-                cluster.select(self.0.round_robin, &self.0.rr_counter, None)
+        let cluster = self.inner.pools.load();
+        if let Some((index, pool)) =
+            cluster.select(self.inner.round_robin, &self.inner.rr_counter, None)
         {
             let failover_task = FailoverTask {
                 last_index: index,
